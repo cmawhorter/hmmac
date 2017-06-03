64d501211b51ebc2881219db09211492a1bab0f5
Cory Mawhorter <cory.mawhorter@gmail.com>
2017-06-03 11:53:48 -0700

    closes #15

--- test/express.js
+++ test/express.js
@@ -11,7 +11,42 @@ var invalidCreds = { key: 'a', secret: '2' };
 
 describe('node express', function() {
 
-  it('should authenticate', function(done) {
+  it('should authenticate GET without body parser', function(done) {
+    var app = express();
+    var hmmac = new Hmmac({
+      credentialProvider: function(key, callback) {
+        callback(validCreds);
+      }
+    });
+
+    app.use(function (req, res, next) {
+      hmmac.validate(req, function(valid) {
+        done(true === valid ? null : new Error('did not validate'));
+      });
+    });
+
+    var server = app.listen(12400, function () {
+      var signedRequest = {
+        host: '127.0.0.1',
+        port: 12400,
+        path: '/',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Date': new Date().toUTCString()
+        }
+      };
+      hmmac.sign(signedRequest, validCreds);
+      var req = http.request(signedRequest, function(res) {
+
+      });
+
+      req.end();
+    });
+  });
+
+
+  it('should authenticate GET with body parser', function(done) {
     var app = express();
     var hmmac = new Hmmac({
       credentialProvider: function(key, callback) {
@@ -27,11 +62,47 @@ describe('node express', function() {
       });
     });
 
-    var server = app.listen(12400, function () {
+    var server = app.listen(12405, function () {
+      var signedRequest = {
+        host: '127.0.0.1',
+        port: 12405,
+        path: '/',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Date': new Date().toUTCString()
+        }
+      };
+      hmmac.sign(signedRequest, validCreds);
+      var req = http.request(signedRequest, function(res) {
+
+      });
+
+      req.end();
+    });
+  });
+
+  it('should authenticate PUT', function(done) {
+    var app = express();
+    var hmmac = new Hmmac({
+      credentialProvider: function(key, callback) {
+        callback(validCreds);
+      }
+    });
+
+    app.use(bodyParser.json());
+
+    app.use(function (req, res, next) {
+      hmmac.validate(req, function(valid) {
+        done(true === valid ? null : new Error('did not validate'));
+      });
+    });
+
+    var server = app.listen(12401, function () {
       var payload = JSON.stringify({ some: 'thing' });
       var signedRequest = {
         host: '127.0.0.1',
-        port: 12400,
+        port: 12401,
         path: '/',
         method: 'PUT',
         body: payload,
@@ -67,11 +138,11 @@ describe('node express', function() {
       });
     });
 
-    var server = app.listen(12401, function () {
+    var server = app.listen(12402, function () {
       var payload = JSON.stringify({ some: 'thing' });
       var signedRequest = {
         host: '127.0.0.1',
-        port: 12401,
+        port: 12402,
         path: '/',
         method: 'PUT',
         body: payload,
